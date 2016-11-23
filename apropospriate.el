6038def2ea76deb2e8299c3bc5fd8cc300d52f88
Anthony Cowley <acowley@gmail.com>
2016-11-22 20:50:37 -0500

    Darken the background color

--- apropospriate.el
+++ apropospriate.el
@@ -41,7 +41,7 @@ Set to `1.0' to prevent font size manipulation."
   (declare (indent 0))
   `(let* ((class '((class color) (min-colors 89)))
           (variant ,variant)
-          (base00   (if (eq variant 'light) "#FAFAFA" "#424242"))
+          (base00   (if (eq variant 'light) "#FAFAFA" "#313131"))
           (base01   (if (eq variant 'light) "#90A4AE" "#757575"))
           (base02   (if (eq variant 'light) "#78909C" "#9E9E9E"))
           (base03   (if (eq variant 'light) "#546E7A" "#E0E0E0"))
